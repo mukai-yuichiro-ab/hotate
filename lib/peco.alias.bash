37b6183a05f18f9049a93b14b5dcf3632fec0f8b
Yuichiro MUKAI <mukai_yuichiro@applibot.co.jp>
2015-10-30 10:27:29 +0900

    implement some plugins & a setup script

---
+++ lib/peco.alias.bash
@@ -0,0 +1,3 @@
+alias pco='git checkout `git branch | peco`'
+alias pcd='cd $(ghq list -p | peco)'
+alias phs='peco_select_history'
---
+++ lib/peco.plugin.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+peco_select_history() {
+    declare l=$(HISTTIMEFORMAT=  history | LC_ALL=C sort -r |  awk '{for(i=2;i<NF;i++){printf("%s%s",$i,OFS=" ")}print $NF}'   |  peco --query "$READLINE_LINE")
+    READLINE_LINE="$l"
+    READLINE_POINT=${#l}
+}
+
